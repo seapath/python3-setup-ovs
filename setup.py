f2b575b7cd1c1954f4338d65a83bc88f62e26746
Florent Carli <florent.carli@rte-france.com>
2025-01-23 11:58:18 +0100

    re-packaging python 3.12
    Move from setup.py to pyproject.toml
    
    Signed-off-by: Florent Carli <florent.carli@rte-france.com>

--- setup.py
+++ setup.py
@@ -3,15 +3,4 @@
 
 from setuptools import setup
 
-setup(
-    name="setup_ovs",
-    version="1.0",
-    packages=["setup_open_vswitch"],
-    author="RTE",
-    license="Apache License 2.0",
-    author_email="mathieu.dupre@savoirfairelinux.com",
-    description="Apply an OVS configuration from a JSON file",
-    scripts=[
-        "setup_ovs.py",
-    ],
-)
+setup()
